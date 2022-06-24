fe2ace00c96be23f0f161ea7e82cc3c993727e26
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-06-25 00:17:23 +0300

    json server added

--- src/api/cartApi.jsx
+++ src/api/cartApi.jsx
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const cartApi = axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: "https://shopmop.herokuapp.com",
 });
 export const getCart = async () => {
-  const response = await cartApi.get('/cart');
+  const response = await cartApi.get("/cart");
   return response.data;
 };
 export const addProduct = async (product) => {
-  return await cartApi.post('/cart', product);
+  return await cartApi.post("/cart", product);
 };
 
 export const deleteProduct = async ({ id }) => {
